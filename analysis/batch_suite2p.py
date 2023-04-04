85c61c0376450e431387f5c3c8ff41446cd6a651
superwhat1 <david.foubertl@gmail.com>
2023-04-04 11:10:49 -0400

    merging

--- analysis/batch_suite2p.py
+++ analysis/batch_suite2p.py
@@ -9,12 +9,11 @@ import numpy as np
 import glob
 from suite2p import run_s2p
 
-ops = np.load('C:/Users/BioCraze/Documents/Ruthazer lab/glial training/anatomical_max_roi_detection_ops.npy', allow_pickle=True).item()
-rootdir = 'C:\\Users\\BioCraze\\Documents\\Ruthazer lab\\glial training\\data'
+ops = np.load('C:/Users/BioCraze/Documents/Ruthazer lab/glia_training/anatomical_max_roi_detection_ops.npy', allow_pickle=True).item()
+rootdir = 'C:\\Users\\BioCraze\\Documents\\Ruthazer lab\\glia_training\\data\\training_02feb23\\'
 
 files =[f.path[:f.path.rfind('\\')+1] for i in glob.glob(f'{rootdir}/*/**/',recursive=True) for f in os.scandir(i) if f.path.endswith('.tif')]
 
-
 for f in files:
     db = {'data_path':[f]}
-    opsEND = run_s2p(ops=ops, db=db) 
+    opsEND = run_s2p(ops=ops, db=db)
